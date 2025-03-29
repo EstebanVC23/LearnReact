5c170f10e82c08d4c91b59bf32887556da56788d
EstebanVC <204529852+EstebanVC23@users.noreply.github.com>
2025-03-29 00:10:21 -0500

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function getRamdomIntInRange(min, max) {
 }
 
 function Header() {
-    const reactWinner = reactTitles[getRamdomIntInRange(0, 4)];
+    const tittleWinner = reactTitles[getRamdomIntInRange(0, 4)];
     return (
         <header>
             <img src="src/assets/react.svg" alt="React logo" />
-            <h1>{reactWinner}</h1>
+            <h1>{tittleWinner}</h1>
             <p>
                 ¡Conceptos fundamentales de React que necesitas conocer para
                 desarrollar cualquier app con esta famosa librería!
